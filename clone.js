603fa99fd1afab1f0931a8572825dc5d65f0d821
webdesigngse <webdesign@gse.nl>
2016-09-19 13:35:55 +0200

    001: clone.js/open + package update

---
+++ clone.js
@@ -0,0 +1,7 @@
+var Git = require('nodegit');
+
+Git.Clone('https://github.com/webdesigngse/gubat22', 'nodegit').then(function(repository){
+
+  /* hier kan gewerkt worden met het repository object */
+
+});
